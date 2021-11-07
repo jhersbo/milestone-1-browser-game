d0c0da513f282b7815b1362e647b60d103561b7e
jhersbo <jhersbo@gmail.com>
2021-11-07 16:48:17 -0600

    reconfigured some things with Mac environment

--- primary.js
+++ primary.js
@@ -15,21 +15,3 @@ $(document).ready( () =>{
     })
 })
 
-//Handles different aspect ratios.
-
-// $('#game-canvas').css({
-//     'height': gameHeight + 'px',
-//     'width': gameWidth + 'px',
-// })
-// $('#modal-content').css({
-//     'width': gameWidth + 'px',
-// })
-/////////////////////////////////
-
-//TODO:
-//1. Build playable character class.
-//2. Build NPC class. Need to figure out customizable options.
-//3. Build the "map" renderer. 
-//4. Build the game interface.
-//5. 
-
